191e5c655108feaa0147a9f0107614e1a46f4f1b
Apoorv Chaudhary <ac.apoorvchaudhary@gmail.com>
2022-01-30 11:56:21 +0530

    added video resource

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from cgi import test
-from tkinter.font import names
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"apoorv": {"age": 25, "gender": "male"},
-        "bill": {"age": 22, "gender": "male"}}
+videos = {}
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name] 
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form['likes'])
+        return {}
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
