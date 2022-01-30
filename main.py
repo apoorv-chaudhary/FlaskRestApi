c622d0c60735723087d410e1f91aaf1c45e089ef
Apoorv Chaudhary <ac.apoorvchaudhary@gmail.com>
2022-01-30 10:10:56 +0530

    displayed stored parameters

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from cgi import test
+from tkinter.font import names
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+names = {"apoorv": {"age": 25, "gender": "male"},
+        "bill": {"age": 22, "gender": "male"}}
+
 class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"name": name, "test": test} 
+    def get(self, name):
+        return names[name] 
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
