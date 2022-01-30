90f70cf9cc92d9bbf727f2bc1bac7adac85697cb
Apoorv Chaudhary <ac.apoorvchaudhary@gmail.com>
2022-01-30 16:40:38 +0530

    querying database and serializing objects

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,36 +17,32 @@ class VideoModel(db.Model):
         return f"Video(name = {name}, views = {views}, likes = {likes})"
 
 
-
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=str, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=str, help="Likes of the video is required", required=True)
 
-videos = {}
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Video not found for given ID...")
-
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video already exists with that ID...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'likes': fields.Integer,
+    'views': fields.Integer
+}
 
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.get(id=video_id)
+        return result
 
     def put(self, video_id):
-        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
 
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
         del videos[video_id]
         return '', 204
 
