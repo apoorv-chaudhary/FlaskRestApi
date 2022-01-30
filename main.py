1e64df9be86b1403304583eadca13d7aae85528a
Apoorv Chaudhary <ac.apoorvchaudhary@gmail.com>
2022-01-30 18:47:33 +0530

    Adding entries to the db

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from email import message
+from unicodedata import name
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -16,6 +18,7 @@ class VideoModel(db.Model):
     def __repr__(self):
         return f"Video(name = {name}, views = {views}, likes = {likes})"
 
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
@@ -34,14 +37,23 @@ resource_fields = {
 class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(id=video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
         return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
+        
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
-
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video ID is already taken...")
+        video = VideoModel(id=video_id, name=args['name'], likes=args['likes'], views=args['views'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+  
     def delete(self, video_id):
         del videos[video_id]
         return '', 204
--- test.py
+++ test.py
@@ -7,15 +7,13 @@ data = [{"likes": 10, "name": "Joe", "views": 1000},
         {"likes": 80000, "name": "How to make a REST API", "views": 1000000}, 
         {"likes": 24, "name": "Apoorv", "views": 11110}]
 
+
 for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
+
 
 input()
-
-response = requests.get(BASE + "video/2")
+response = requests.get(BASE + "video/6")
 print(response.json())
